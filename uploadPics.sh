15b3d9dec855fb810e11815f98e39f2fe17fd81a
Yang, Ruijie <yrjie0@gmail.com>
2015-09-07 09:49:05 +0900

    added picture rescale

--- uploadPics.sh
+++ uploadPics.sh
@@ -4,6 +4,10 @@ then
     exit
 fi
 
-find . -name '.DS_Store' -exec rm {} \;
+export PATH=$PATH:~/bin/
+#convert -resize 450x600 static/data/20150903/lunch9/P1100508_1.JPG static/data/20150903/lunch9/P1100508_1.JPG
+find $1 -name '*.JPG' -exec convert -rotate 90 {} {} \;
+find $1 -name '*.JPG' -exec convert -resize 600x800 {} {} \;
+find $1 -name '.DS_Store' -exec rm {} \;
 scp -r $1 azureuser@gcsd:~/Rghoul/static/data/
 curl http://rakuten-ghoul.xyz/update/
