e70aeaa4ddd78ab4dfebc6442c9331823ace7612
Yang, Ruijie <yrjie0@gmail.com>
2015-09-07 22:32:32 +0900

    change upload to JP server

--- uploadPics.sh
+++ uploadPics.sh
@@ -6,8 +6,9 @@ fi
 
 export PATH=$PATH:~/bin/
 #convert -resize 450x600 static/data/20150903/lunch9/P1100508_1.JPG static/data/20150903/lunch9/P1100508_1.JPG
-find $1 -name '*.JPG' -exec convert -rotate 90 {} {} \;
+#find $1 -name '*.JPG' -exec convert -rotate 90 {} {} \;
+find $1 -name '*.JPG' -exec convert -auto-orient {} {} \;
 find $1 -name '*.JPG' -exec convert -resize 600x800 {} {} \;
 find $1 -name '.DS_Store' -exec rm {} \;
-scp -r $1 azureuser@gcsd:~/Rghoul/static/data/
+scp -r $1 azureuser@gcsdJP:~/Rghoul/static/data/
 curl http://rakuten-ghoul.xyz/update/
