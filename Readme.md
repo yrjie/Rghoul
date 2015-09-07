c78e02698606f196f1e378bd42577c9733194361
azureuser <azureuser@gcsd-id-mars-jp.gcsd-id-mars-japan.l5.internal.cloudapp.net>
2015-09-07 12:53:07 +0000

    updated readme

--- Readme.md
+++ Readme.md
@@ -2,3 +2,10 @@ gcsd-id-mars.cloudapp.net
 
 python 2.7.3
 django 1.7.10
+
+dependencies
+  zlib-devel.x86_64
+  sqlite-devel.x86_64
+  openssl-devel.x86_64
+  readline-devel.x86_64
+  python-dateutil
--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
 alias python=/usr/local/bin/python2.7
-export PYTHONPATH=$PYTHON:/usr/lib/python2.7/site-packages/
+#export PYTHONPATH=$PYTHON:/usr/lib/python2.7/site-packages/
 logF=log/`date +"%Y%m%d"`.log
-python manage.py runserver --insecure 100.72.188.107:80 >>$logF 2>&1
+python manage.py runserver --insecure 100.72.198.81:80 >>$logF 2>&1
