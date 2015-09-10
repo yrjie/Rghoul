1bf15959aadfb2de800a0897a0646f39da72654e
yrjie <azureuser@gcsd-id-mars.gcsd-id-mars.b7.internal.cloudapp.net>
2015-09-10 05:08:17 +0000

    script for gcsd_us (test)

--- runserver_test.sh
+++ runserver_test.sh
@@ -1,1 +1,4 @@
-python manage.py runserver --insecure 137.132.82.240:8080
+alias python=/usr/local/bin/python2.7
+#export PYTHONPATH=$PYTHON:/usr/lib/python2.7/site-packages/
+logF=log/`date +"%Y%m%d"`.log
+python manage.py runserver --insecure 100.72.102.16:80 >>$logF 2>&1
