7d6080763c885f2bd706696afbf9de3f67085162
ruijie <ruijie@genome.ddns.comp.nus.edu.sg>
2015-10-03 23:09:39 +0800

    fixed admin

--- Rghoul/admin.py
+++ Rghoul/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Picture, Comment
+from models import Picture, Comment, Dish
 
 class PictureAdmin(admin.ModelAdmin):
     list_display = ("picName", "date", "mealTime", "floor", "like", "dislike")
