b1471b0c1b69b0248b18ab1b2b0d936af94cf257
azureuser <azureuser@gcsd-id-mars-jp.gcsd-id-mars-japan.l5.internal.cloudapp.net>
2015-12-01 12:39:48 +0000

    used 8001

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,5 @@
 alias python=/usr/local/bin/python2.7
 #export PYTHONPATH=$PYTHON:/usr/lib/python2.7/site-packages/
 logF=log/`date +"%Y%m%d"`.log
-python manage.py runserver --insecure 100.74.102.130:80 >>$logF 2>&1
+#python manage.py runserver --insecure 100.74.102.130:80 >>$logF 2>&1
+python manage.py runserver --insecure 0.0.0.0:8001 >>$logF 2>&1
