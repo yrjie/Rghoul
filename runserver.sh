e726483a7345251e3d218519a27507584deb2b2b
Yang, Ruijie <yrjie0@gmail.com>
2016-03-03 09:54:06 +0900

    added changes for cloud foundry

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,10 @@
-alias python=/usr/local/bin/python2.7
+#alias python=/usr/local/bin/python2.7
 #export PYTHONPATH=$PYTHON:/usr/lib/python2.7/site-packages/
 logF=log/`date +"%Y%m%d"`.log
 #python manage.py runserver --insecure 100.74.102.130:80 >>$logF 2>&1
-python manage.py runserver --insecure 0.0.0.0:8001 >>$logF 2>&1
+if [[ -z $VCAP_APP_PORT ]]  # is empty
+then
+    VCAP_APP_PORT=5000
+fi
+echo 'running at ' 0.0.0.0:$VCAP_APP_PORT
+python manage.py runserver --insecure 0.0.0.0:$VCAP_APP_PORT >>$logF 2>&1
